13657ba32907b3b863c60fd64f9f4f938127a3cf
Youngjae Seo <yoseo@c2r7s3.42seoul.kr>
2022-08-16 23:07:13 +0900

    asd

---
+++ ray/cub3D.h
@@ -0,0 +1,20 @@
+typedef struct s_vf2d
+{
+	float x;
+	float y;
+}	t_vf2d;
+
+typedef struct s_vi2d
+{
+	int	x;
+	int	y;
+}	t_vi2d;
+
+typedef struct s_raycast_hit
+{
+	float dist;
+	char is_hit;
+	char type;
+}	t_racast_hit;
+
+
