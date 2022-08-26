e6a051c66f0a22636c6e6f372ca7505a1d82a6ec
Youngjae Seo <yoseo@c3r1s1.42seoul.kr>
2022-08-26 18:37:53 +0900

    norm vector_helper.c

--- cub3d/src/vector_helperd.c
+++ cub3d/src/vector_helperd.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector_helperd.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoseo <yoseo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 18:37:06 by yoseo             #+#    #+#             */
+/*   Updated: 2022/08/26 18:37:10 by yoseo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 t_vf2d	new_vf2d(float x, float y)
@@ -29,10 +41,10 @@ t_vf2d	rad_to_dir(float radin)
 
 t_vf2d	vf2d_multi(t_vf2d vf2d, float mul)
 {
-	return new_vf2d(vf2d.x * mul, vf2d.y * mul);
+	return (new_vf2d(vf2d.x * mul, vf2d.y * mul));
 }
 
-t_vf2d vf2d_add(t_vf2d a, t_vf2d b)
+t_vf2d	vf2d_add(t_vf2d a, t_vf2d b)
 {
-	return new_vf2d(a.x + b.x, a.y + b.y);
+	return (new_vf2d(a.x + b.x, a.y + b.y));
 }
