8fa966dc046c09a83d9d3ebc4fe4ab39b305c5be
Youngjae Seo <yoseo@c3r1s1.42seoul.kr>
2022-08-26 18:45:37 +0900

    norm img_draw, raycasting.c, img_init.c, rendering_world.c

--- cub3d/src/rendering_world.c
+++ cub3d/src/rendering_world.c
@@ -14,7 +14,7 @@
 
 t_image	*texture_init(t_raycast_hit hit, t_object *player)
 {
-	t_image *texture;
+	t_image	*texture;
 
 	if (hit.wall_dir == NO)
 		texture = &player->no_texture;
@@ -24,44 +24,45 @@ t_image	*texture_init(t_raycast_hit hit, t_object *player)
 		texture = &player->we_texture;
 	else
 		texture = &player->ea_texture;
-	return texture;
+	return (texture);
 }
 
 static void	rendering_box(t_object *player, t_raycast_hit hit, t_vf2d ho, int x)
 {
-		float tm;
-		float tx;
-		float sstep;
-		t_image *tt;
-		
-		tt = texture_init(hit, player);
-		if (hit.is_disth == 1)
-		{
-			tm = (int)(((hit.hit_point.y) - (int)(hit.hit_point.y)) * tt->height);
-			sstep = (float)(tt->height) / ho.x;
-		}
-		else
-		{
-			tm = (int)(((hit.hit_point.x) - (int)(hit.hit_point.x)) * tt->width);
-			sstep = (float)(tt->width) / ho.x;
-		}
-		for (int y = ho.y; y < (int)(ho.x + ho.y); y++)
-		{
-			if (y >= player->image->height)
-				break;
-			int co = img_get_color(tt, (int)tm % tt->width, (int)tx % tt->height);
-			img_draw_pixel(player->image, x, y, co);
-			tx = tx + sstep;
-		}	
+	float	tm;
+	float	tx;
+	float	sstep;
+	t_image	*tt;
+	int		color;
+
+	tt = texture_init(hit, player);
+	if (hit.is_disth == 1)
+	{
+		tm = (int)(((hit.hit_point.y) - (int)(hit.hit_point.y)) * tt->height);
+		sstep = (float)(tt->height) / ho.x;
+	}
+	else
+	{
+		tm = (int)(((hit.hit_point.x) - (int)(hit.hit_point.x)) * tt->width);
+		sstep = (float)(tt->width) / ho.x;
+	}
+	for (int y = ho.y; y < (int)(ho.x + ho.y); y++)
+	{
+		if (y >= player->image->height)
+			break ;
+		color = img_get_color(tt, (int)tm % tt->width, (int)tx % tt->height);
+		img_draw_pixel(player->image, x, y, color);
+		tx = tx + sstep;
+	}	
 }
 
 void	rendering_word(t_object *player)
 {
-	t_raycast_hit hit;
-	t_vf2d	ho;
-	int i;
-	float ra;
-	float step;
+	t_raycast_hit	hit;
+	t_vf2d			ho;
+	int				i;
+	float			ra;
+	float			step;
 
 	i = 0;
 	ra = player->pa - (CAMERA_RANGE / 2.0f) * RADIN;
@@ -69,13 +70,16 @@ void	rendering_word(t_object *player)
 	while (i < WINDOW_SIZE_X)
 	{
 		ra = safe_ra(ra);
-		hit = raycasting(&player->map_info, player->position, rad_to_dir(ra), 15);
+		hit = raycasting(&player->map_info,
+				player->position, rad_to_dir(ra), 15);
 		ho.x = (WINDOW_SIZE_Y / 1.5f) / (hit.distance * cosf(ra - player->pa));
-        ho.y = ((WINDOW_SIZE_Y) / 2) - ho.x / 2;
-		img_draw_fill_rectangle(player->image, new_vi2d(i, 0), new_vi2d(1, ho.y), 0X0067E3);
+		ho.y = ((WINDOW_SIZE_Y) / 2) - ho.x / 2;
+		img_draw_fill_rectangle(player->image,
+			new_vi2d(i, 0), new_vi2d(1, ho.y), 0X0067E3);
 		rendering_box(player, hit, ho, i);
-		img_draw_fill_rectangle(player->image, new_vi2d(i, ho.x + ho.y),
-								new_vi2d(1, WINDOW_SIZE_Y - ho.x + ho.y), 0X808080);
+		img_draw_fill_rectangle(player->image,
+			new_vi2d(i, ho.x + ho.y),
+			new_vi2d(1, WINDOW_SIZE_Y - ho.x + ho.y), 0X808080);
 		ra += RADIN / step;
 		++i;
 	}
