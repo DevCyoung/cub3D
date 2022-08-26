ab108fe868e7aa4b9a4a61775a30699bf2ff20ba
Youngjae Seo <yoseo@c3r1s1.42seoul.kr>
2022-08-26 19:32:11 +0900

    norm rendering_minmap.c

--- cub3d/src/rendering_minimap.c
+++ cub3d/src/rendering_minimap.c
@@ -6,7 +6,7 @@
 /*   By: yoseo <yoseo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 18:47:27 by yoseo             #+#    #+#             */
-/*   Updated: 2022/08/26 19:19:23 by yoseo            ###   ########.fr       */
+/*   Updated: 2022/08/26 19:25:03 by yoseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static int	map_color(t_object *player, t_vi2d pixel)
 {
 	t_map_info	map_info;
-	int color			;
+	int			color;
 
 	map_info = player->map_info;
 	if (pixel.x < 0 || pixel.x >= MAP_X || pixel.y < 0 || pixel.y >= MAP_Y)
@@ -36,37 +36,37 @@ static void	rendering_player(t_object *player)
 
 	image = player->image;
 	p_size = (float)(WINDOW_SIZE_X / MINIMAP_SIZE) / 20;
-	start = new_vi2d(((WINDOW_SIZE_X / MINIMAP_SIZE) / 2) - p_size / 2, ((WINDOW_SIZE_Y / MINIMAP_SIZE) / 2) - p_size / 2);
+	start = new_vi2d(((WINDOW_SIZE_X / MINIMAP_SIZE) / 2) - p_size / 2,
+			((WINDOW_SIZE_Y / MINIMAP_SIZE) / 2) - p_size / 2);
 	len = new_vi2d(p_size, p_size);
 	img_draw_fill_rectangle(image, start, len, 0X00FFFF00);
 }
 
 void	rendering_minimap(t_object *player)
 {
-	t_image *image;
-	t_map_info map_info;
-	
-	map_info = player->map_info;
-	image = player->image;
+	t_vi2d	idx;
+	t_vf2d	map_step;
+	t_vf2d	offset;
 
-	int y = 0;
-	float map_step_x =  MINIMAP_BOX_SIZE / (float)(WINDOW_SIZE_X / MINIMAP_SIZE);
-	float map_step_y =  MINIMAP_BOX_SIZE / (float)(WINDOW_SIZE_Y / MINIMAP_SIZE);
-	t_vf2d offset = new_vf2d(player->position.x - 4.0f, player->position.y - 4.0f);
-	
-	y = 0;
-	while (y < WINDOW_SIZE_Y / MINIMAP_SIZE)
+	idx = new_vi2d(0, 0);
+	map_step.x = MINIMAP_BOX_SIZE / (float)(WINDOW_SIZE_X / MINIMAP_SIZE);
+	map_step.y = MINIMAP_BOX_SIZE / (float)(WINDOW_SIZE_Y / MINIMAP_SIZE);
+	offset = new_vf2d(player->position.x - 4.0f, player->position.y - 4.0f);
+	while (idx.y < WINDOW_SIZE_Y / MINIMAP_SIZE)
 	{
-		int x = 0;
 		offset.x = player->position.x - 4.0f;
-		while (x < WINDOW_SIZE_X / MINIMAP_SIZE)
+		idx.x = 0;
+		while (idx.x < WINDOW_SIZE_X / MINIMAP_SIZE)
 		{
-			img_draw_pixel(image, x, y, alpha_blend(map_color(player, new_vi2d((int)offset.x, (int)offset.y)), img_get_color(image, x, y)));
-			offset.x += map_step_x;
-			++x;
+			img_draw_pixel(player->image, idx.x, idx.y,
+				alpha_blend(map_color(player,
+						new_vi2d((int)offset.x, (int)offset.y)),
+					img_get_color(player->image, idx.x, idx.y)));
+			offset.x += map_step.x;
+			++idx.x;
 		}
-		offset.y += map_step_y;
-		++y;
+		offset.y += map_step.y;
+		++idx.y;
 	}
 	rendering_player(player);
 }
